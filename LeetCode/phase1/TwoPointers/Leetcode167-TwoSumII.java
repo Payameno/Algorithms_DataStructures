990eb17cd94c179c5d3e92058d4824e9dd9497d4
Payam Beigi <beigi.payam@gmail.com>
2023-07-13 19:12:32 -0700

    TwoPointers Sum||

---
+++ LeetCode/phase1/TwoPointers/Leetcode167-TwoSumII.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Solution {
+
+    public int[] twoSum(int[] numbers, int target) {
+      int len = numbers.length;
+      int L = 0, R = len-1;
+
+      while (R<L) {
+        int curSum = numbers[L] + numbers[R];
+
+        if (curSum==target) {
+          return new int[] {L+1, R+1};
+        } else if (curSum>target) {
+          R--;
+        } else {
+          L--;
+        }
+
+      }
+      return new int[] {-1,-1};
+    }
+
+}
+
+/*
+ * Input: numbers = [2,7,11,15], target = 9
+ *Output: [1,2]
+ */
