3df8c1e1db845714d426f4ea007cc271733e4192
Payam Beigi <beigi.payam@gmail.com>
2023-09-08 17:26:09 -0700

    Solve binarySearch leetcode 94, 144

---
+++ LeetCode/phase2/BinaryTree/94BinaryTreeInorderTraversalIterative.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution4 {
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> res = new LinkedList<>();
+
+        //Base Case
+        if (root == null) return res;
+
+        //Iterative traversal of the binary tree
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+
+        while (cur != null || !stack.isEmpty()) {
+            
+            if (cur == null) {
+                TreeNode top = stack.pop();
+                res.add(top.val);
+                cur = top.right;
+
+            } else {
+                stack.add(cur);
+                cur = cur.left;
+            }
+        }
+
+        return res;    
+    }
+
+}
+
+/*
+ * Iteretive inorder binary tree traversal
+ * 
+ * Time complexity: O(n) 
+ * 
+ * Space complexity: O(h) where h is the height of the binary tree
+ * worse case in the case of a linkedList o(n)
+ */
---
+++ LeetCode/phase2/BinaryTree/94BinaryTreeInorderTraversalRecursive.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    List<Integer> res = new LinkedList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        //In-order traversal by recursive approach
+        dfs(root);
+
+        return res;
+    }
+
+    private void dfs(TreeNode node) {
+        
+        if (node == null) return;
+
+        //Traverse the left subtree
+        dfs(node.left);
+
+        //Add the value of the node
+        res.add(node.val);
+
+        //traverse the right subtree
+        dfs(node.right);
+    }
+}
