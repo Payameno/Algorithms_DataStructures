45c8958a09be02b0f50e747be826d07575ac9419
Payam Beigi <beigi.payam@gmail.com>
2023-11-20 17:04:42 -0800

    Solved leetcode 76 sliding window technique to find min window substring

---
+++ LeetCode/phase1/SlidingWindow/76minimumwindowsubstring.java
@@ -0,0 +1,99 @@
+package SlidingWindow;
+
+class Solution {
+
+    public String minWindow(String s, String t) {
+
+        //ASCI characters, initialized as 0s
+        int[] map = new int[128];
+        char[] arr = s.toCharArray();
+
+        //Deifne pointers
+        int left = 0, right = 0, n = arr.length;
+        int minLength = Integer.MAX_VALUE, countSubStringChar = 0;
+        String minLengthString = "";
+
+        //Map substring
+        for (char cur : t.toCharArray()) {
+            map[cur]++;
+        }
+
+        //Perform iterations through the character array
+        while (right < n) {
+
+            //Operation
+            map[arr[right]]--;
+            if (0 <= map[arr[right]]) {
+                countSubStringChar++;
+            }
+
+
+            //Shrin the window if it contains all characters
+            while (countSubStringChar == t.toCharArray().length) {
+                if (minLength > right - left + 1) {
+                    minLength = right - left + 1;
+                    minLengthString = s.substring(left, right + 1);
+                }
+
+
+            //shrink the window
+            map[arr[left]]++;
+            if (0 < map[arr[left]]) {
+                countSubStringChar--;
+            }
+            left++;
+
+        }
+
+        right++;
+
+        }
+        return minLengthString;
+    }
+
+}
+
+/*
+
+Time complexity:
+
+Space complexity:
+
+                
+                 Input: s = "ADOBECODEBANC", t = "ABC"
+                                      ----
+                  A = 0
+                  B = 0
+                  C = 0
+                  count 3
+                  BANC
+
+
+Example 1:
+
+Input: s = "ADOBECODEBANC", t = "ABC"
+Output: "BANC"
+Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
+Example 2:
+
+Input: s = "a", t = "a"
+Output: "a"
+Explanation: The entire string s is the minimum window.
+Example 3:
+
+Input: s = "a", t = "aa"
+Output: ""
+Explanation: Both 'a's from t must be included in the window.
+Since the largest window of s only has one 'a', return empty string.
+ 
+
+Constraints:
+
+m == s.length
+n == t.length
+1 <= m, n <= 105
+s and t consist of uppercase and lowercase English letters.
+ 
+
+Follow up: Could you find an algorithm that runs in O(m + n) time?
+ */
